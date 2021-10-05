4874f36d14f9924a3b04a5190e9ffb78360cbe3b
NathanIsBored13 <54941652+NathanIsBored13@users.noreply.github.com>
2021-10-05 02:36:11 +0100

    Added support for multiple controllers

---
+++ XBControllerViewer/ViewModel/ControllerIOViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using XBControllerViewer.Core;
+using XBControllerViewer.Model;
+
+namespace XBControllerViewer.ViewModel
+{
+    public class ControllerIOViewModel
+    {
+        public uint ControllerIndex { get; }
+
+        public XBController Controller { get; }
+
+        public ObservableCollection<IOBindingModel> Bindings { get; }
+
+        public IOBindingModel SelectedBinding { get; set; }
+
+        public RelayCommand CreateBinding { get; }
+        public RelayCommand RemoveBinding { get; }
+
+        public string InputBindingSelection { get; set; }
+        public string OutputBindingName { get; set; }
+
+        public Dictionary<string, int> EvaluateBindings
+        {
+            get
+            {
+                Dictionary<string, int> ret = new Dictionary<string, int>();
+
+                foreach (IOBindingModel binding in Bindings)
+                {
+                    ret.Add(binding.Output, Controller.Getters[binding.Input].Invoke());
+                }
+
+                return ret;
+            }
+        }
+
+        public ControllerIOViewModel(uint controller_index)
+        {
+            ControllerIndex = controller_index;
+
+            Controller = new XBController();
+
+            Bindings = new ObservableCollection<IOBindingModel>();
+
+            CreateBinding = new RelayCommand(
+            (param) =>
+            {
+                if (!Bindings.Any(binding => binding.Input == InputBindingSelection) && !Bindings.Any(binding => binding.Output == OutputBindingName))
+                {
+                    Bindings.Add(new IOBindingModel()
+                    {
+                        Input = InputBindingSelection,
+                        Output = OutputBindingName
+                    });
+                }
+            });
+
+            RemoveBinding = new RelayCommand(
+            (param) =>
+            {
+                _ = Bindings.Remove(SelectedBinding);
+            });
+
+            Task update = new Task(() =>
+            {
+                while (true)
+                {
+                    if (XBControllerInterface.IsConnected(ControllerIndex) && XBControllerInterface.HasData(ControllerIndex))
+                    {
+                        XBControllerInterface.XBControllerState state = XBControllerInterface.GetState(ControllerIndex);
+                        Controller.SetState(state);
+                    }
+                }
+            });
+            update.Start();
+        }
+    }
+}
--- XBControllerViewer/ViewModel/MainViewModel.cs
+++ XBControllerViewer/ViewModel/MainViewModel.cs
@@ -1,38 +1,22 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using XBControllerViewer.Core;
-using XBControllerViewer.Model;
 
 namespace XBControllerViewer.ViewModel
 {
     public class MainViewModel : ObservableObject
     {
-        public XBController Controller { get; }
-
+        public List<ControllerIOViewModel> Controllers { get; }
         public RelayCommand OnExit { get; }
 
-        public ObservableCollection<IOBindingModel> Bindings { get; }
-
-        public IOBindingModel SelectedBinding { get; set; }
-
-        public RelayCommand CreateBinding { get; }
-        public RelayCommand RemoveBinding { get; }
-
-        public string InputBindingSelection { get; set; }
-        public string OutputBindingName { get; set; }
-
-        public Dictionary<string, int> EvaluateBindings
+        public Dictionary<string, Dictionary<string, int>> EvaluateBindings
         {
             get
             {
-                Dictionary<string, int> ret = new Dictionary<string, int>();
+                Dictionary<string, Dictionary<string, int>> ret = new Dictionary<string, Dictionary<string, int>>();
 
-                foreach (IOBindingModel binding in Bindings)
+                foreach (ControllerIOViewModel controller in Controllers)
                 {
-                    ret.Add(binding.Output, Controller.Getters[binding.Input].Invoke());
+                    ret.Add(controller.ControllerIndex.ToString(), controller.EvaluateBindings);
                 }
 
                 return ret;
@@ -41,9 +25,11 @@ namespace XBControllerViewer.ViewModel
 
         public MainViewModel()
         {
-            Controller = new XBController();
-
-            Bindings = new ObservableCollection<IOBindingModel>();
+            Controllers = new List<ControllerIOViewModel>();
+            for (uint i = 0; i < 4; i++)
+            {
+                Controllers.Add(new ControllerIOViewModel(i));
+            }
 
             OnExit = new RelayCommand(
             (param) =>
@@ -52,37 +38,6 @@ namespace XBControllerViewer.ViewModel
                 XBControllerInterface.Stop();
             });
 
-            CreateBinding = new RelayCommand(
-            (param) =>
-            {
-                if (!Bindings.Any(binding => binding.Input == InputBindingSelection) && !Bindings.Any(binding => binding.Output == OutputBindingName))
-                {
-                    Bindings.Add(new IOBindingModel()
-                    {
-                        Input = InputBindingSelection,
-                        Output = OutputBindingName
-                    });
-                }
-            });
-
-            RemoveBinding = new RelayCommand(
-            (param) =>
-            {
-                _ = Bindings.Remove(SelectedBinding);
-            });
-
-            Task update = new Task(() =>
-            {
-                while (true)
-                {
-                    if (XBControllerInterface.IsConnected(0) && XBControllerInterface.HasData(0))
-                    {
-                        XBControllerInterface.XBControllerState state = XBControllerInterface.GetState(0);
-                        Controller.SetState(state);
-                    }
-                }
-            });
-            update.Start();
 
             XBControllerInterface.Start();
         }
